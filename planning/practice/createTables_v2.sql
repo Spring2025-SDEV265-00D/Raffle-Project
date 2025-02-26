99ba2dee5df87f8bb1826c1441b4ed681504a67f
souzacts <souzacts@outlook.com>
2025-02-26 16:05:17 -0500

    modified instructor-supplied db schema

---
+++ planning/practice/createTables_v2.sql
@@ -0,0 +1,55 @@
+
+PRAGMA foreign_keys = ON;
+--It seems this needs to be executed every time a session starts
+--SQLITE does not enforce FK by default
+--need to find appropriate place for this 
+
+--need to understand more of daily operations to add CASCADE if needed.
+
+CREATE TABLE IF NOT EXISTS event (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    location TEXT NOT NULL,
+    start_date TEXT NOT NULL,
+    end_date TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS race (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event_id INTEGER NOT NULL,
+    race_number INTEGER NOT NULL CHECK (race_number > 0),
+    closed INTEGER DEFAULT 0 CHECK (closed IN (0, 1)), --Sqlite does not support boolean, we simulate it using int with a constraint
+
+    FOREIGN KEY (event_id) REFERENCES event(id)
+);
+
+CREATE TABLE IF NOT EXISTS horse (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    race_id INTEGER NOT NULL,
+    horse_number INTEGER NOT NULL CHECK (horse_number > 0),
+    winner INTEGER DEFAULT 0 CHECK (winner IN (1, 0)), 
+    scratch INTEGER DEFAULT 0 CHECK (scratch IN (1, 0)), 
+
+    FOREIGN KEY (race_id) REFERENCES race(id)
+);
+
+CREATE TABLE IF NOT EXISTS ticket (
+    reference_number INTEGER PRIMARY KEY AUTOINCREMENT, --subject to change upon client requirements
+
+    --included race_id so tickets are directly linked to races
+    --Accounts for the case when a horse participates in more than one race.
+    --Avoids the need for joining horse table for ticket information queries.
+    --Might not be needed if its an one time thing for each horse
+    race_id INTEGER NOT NULL,
+    horse_id INTEGER NOT NULL,
+    created_dttm TEXT DEFAULT (datetime('now', 'localtime')),
+
+    
+    status_code INTEGER NOT NULL DEFAULT 0 CHECK (status_code IN (0, 1, 2)),
+    --0: "Issued/Valid"
+    --1: "Redeemed"
+    --2: "Void/Canceled"
+
+
+    FOREIGN KEY (horse_id) REFERENCES horse(id),
+    FOREIGN KEY (race_id) REFERENCES race(id)
+);
