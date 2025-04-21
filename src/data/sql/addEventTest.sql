6261418b3b414e72759f215782f3142ec405e926
souzacts <souzacts@outlook.com>
2025-04-21 10:43:17 -0400

    Delete src/data/sql directory
    moved v1 sql to secondary planning repo

--- src/data/sql/addEventTest.sql
+++
@@ -1,6 +0,0 @@
-insert into event (event_name, location, start_date, end_date)
-values 
-
-('Lilly Fair 2022','Lafayette', '2022-01-05', '2022-01-08'),
-('Hoosier Fest 2025', 'Indianapolis', '2025-01-01', '2025-01-03'); 
---('St Patrick Fair 2022','Lafayette', '2022-01-01', '2022-01-03')
